fb05339d2a0d1cc77126e7d5d74cd6634b78453d
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-22 11:53:31 -0700

    adds edits for more columns in supabase, and rendering an li instead of p to make lists for the nested arrays

--- src/Genre.js
+++ src/Genre.js
@@ -1,8 +1,8 @@
-export default function Genres({ type, description, example }) {
+export default function Genres({ type, description, example, more_like_this }) {
   return (
     <div className="genre-item">
       <h2>{type}</h2>
-      <p>{description} An example of this type of genre would be {example}.</p>
+      <p>{description} An example of this type of genre would be {example}. If you like this, another genre you might also like is {more_like_this}.</p>
     </div>
   );
 }
--- src/Host.js
+++ src/Host.js
@@ -1,8 +1,11 @@
-export default function Stars({ name, from, known_for }) {
+export default function Stars({ name, pronouns, from, known_for }) {
   return (
     <div className="show-item">
       <h2>{name}</h2>
-      <p>From {from}, this host is most well known for {known_for}.</p>
+      <p>From {from}, this host is most well known for {pronouns} work in:</p>
+      <ul>
+        {known_for.map((known_for, i) => <li key={known_for + i}>{known_for}</li>)}
+      </ul>
     </div>
   );
 }
